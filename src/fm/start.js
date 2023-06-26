361f2e80afa45bc2388f202d510c3bea9230ceba
DmVk37 <dmytro_vikharev@epam.com>
2023-06-27 02:59:27 +0300

    update start

--- src/fm/start.js
+++ src/fm/start.js
@@ -1,6 +1,39 @@
 import os from 'node:os';
 import fs from 'node:fs';
-import util from 'node:util';
-import { welcomeMessage, getUserName } from "../utils.js"
 
-console.log(util.format(welcomeMessage), getUserName());
+// import { createReadStream } from 'node:fs';
+
+import { join } from 'node:path';
+
+import util from 'node:util';
+import { welcomeMessage, statusMessage, goodbuyMessage } from "../utils.js"
+import { getPath, getUserName } from '../utils.js';
+
+
+const initialDir = process.env.HOME;
+const currentDir = process.cwd();
+const username = getUserName();
+
+
+
+
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  var input = process.stdin.read();
+  // while(true) {
+    if(input !== null) {
+  
+      var command = input.trim();
+
+      
+
+      if(command === '.exit') {
+        console.log(util.format(goodbuyMessage), username);
+        process.exit(0);
+      }
+    }
+  // }
+});
+
+console.log(util.format(welcomeMessage), username);
+console.log(util.format(statusMessage), initialDir);
